6aa8396bdb9b6a0b0cbb077e64c61a8bcd083f99
SeungWoo Ryu <swstar21c@gmail.com>
2024-12-04 19:04:41 +0900

    모듈 간의 의존성 확인 (2) - Spring Bean에 대한 참조: scanBasePackages 옵션 사용
    - @SpringBootApplication 어노테이션 중 `scanBasePackages` 옵션을 사용해 컴포넌트 스캔이 이루어질 패키지 경로를 명시할 수 있다.
    - 애플리케이션의 패키지 위치를 변경시키는 방식이 아닌, 실제 기업에서 사용하는 방식이다.

--- module-api/src/main/java/com/ncookie/moduleapi/ModuleApiApplication.java
+++ module-api/src/main/java/com/ncookie/moduleapi/ModuleApiApplication.java
@@ -1,9 +1,12 @@
-package com.ncookie;
+package com.ncookie.moduleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication (
+        // 컴포넌트 스캔이 이루어질 패키지를 명시
+        scanBasePackages = { "com.ncookie.moduleapi", "com.ncookie.modulecommon" }
+)
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
